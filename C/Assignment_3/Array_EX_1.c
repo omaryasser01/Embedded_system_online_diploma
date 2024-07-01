6662e4257ce9996ccbabc1d290c15fe1d4af9fbf
Omar Yasser <omaryasser5455@gmail.com>
2024-07-01 12:18:03 +0300

    Array_EX_1.c

--- C/Assignment_3/Array_EX_1.c
+++ C/Assignment_3/Array_EX_1.c
@@ -13,24 +13,35 @@
 
 int main(void)
 {
-	float a[2][2],b[2][2], sum [2][2] ;
-	printf("enter elements of first array\n");
-	for(int i=0; i<2;i++)
-	{ for (int j=0;j<2;j++)
-		{fflush(stdin); fflush(stdout);
-		scanf("%f",&a[i][j]);}
-	}
-	printf("enter elements of second array\n");
-	for(int e=0; e<2;e++)
-		{ for (int q=0;q<2;q++)
-			{fflush(stdin); fflush(stdout);
-			scanf("%f",&b[e][q]);}
-		}
-	for(int z=0; z<2;z++)
+	float a[2][2],b[2][2],sum[2][2];
+	printf("enter elements of first matrix \n");
+	for (int x=0;x<2;x++)
 	{
-			 for (int x=0;x<2;x++)
-				{sum[z][x]=a[z][x]+b[z][x];
-			    printf("%f\n",sum[z][x]);
-			  }
+		for (int y=0;y<2;y++)
+		{
+			printf("Enter a%d%d ",x+1,y+1);
+			fflush(stdin);  fflush(stdout);
+			scanf("%f",&a[x][y]);
+		}
+	}
+	printf("enter elements of second matrix \n");
+	for (int w=0;w<2;w++)
+	{
+		for (int z=0;z<2;z++)
+		{
+			printf("Enter b%d%d ",w+1,z+1);
+			fflush(stdin);  fflush(stdout);
+			scanf("%f",&b[w][z]);
+		}
+	}
+	printf("sum of matrix: \n");
+	for (int c=0;c<2;c++)
+	{
+		for(int v=0;v<2;v++)
+		{
+			sum[c][v]=a[c][v]+b[c][v];
+			printf("%f\t",sum[c][v]);
+		}
+		printf("\n");
 	}
 }
